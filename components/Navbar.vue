9fb5c7acba8d0466b9e57826d2e5a43bd1ed6631
Julian Orozco <orozco.castorena@outlook.com>
2018-12-23 16:43:59 -0600

    Add bootstrap and login form

---
+++ components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nuxt-link
+      to="/"
+      class="navbar-brand"
+    >
+      FrontEnd
+    </nuxt-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div
+      id="navbarSupportedContent"
+      class="collapse navbar-collapse"
+    >
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link
+            to="/"
+            class="nav-link"
+          >
+            Home
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link
+            to="/"
+            class="nav-link"
+          >
+            Posts
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link
+            to="/login"
+            class="nav-link"
+          >
+            Login
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link
+            to="/register"
+            class="nav-link"
+          >
+            Register
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,28 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+      }
+    ],
+    script: [
+      {
+        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+        type: 'text/javascript'
+      },
+      {
+        src:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
+        type: 'text/javascript'
+      }
     ]
   },
 
@@ -26,14 +47,12 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
